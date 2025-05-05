f15f0c23396078a37aac32ae4d688dd79a297026
Sattar-mazouzi <abdesattarmazouzi@gmail.com>
2025-05-05 21:02:37 +0100

    edite

--- src/functions/bearing_stiffness.m
+++ src/functions/bearing_stiffness.m
@@ -1,5 +1,5 @@
 function stiffness = bearing_stiffness(arg)
         % Bearing defect frequency (Hz)
-    stiffness = 1e-2 + 1e-3 * sin(2 * pi * arg(2) * arg(1));
+    stiffness = 1e-1 + 1e-2 * sin(2 * pi * arg(2) * arg(1));
 end
 
